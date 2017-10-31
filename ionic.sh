315ac390ef35f6d860e861d6d2ff855693e87b49
Billy Mahmood <billy@e3creative.co.uk>
2017-10-31 10:44:43 +0000

    add ionic commands

---
+++ ionic.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+## run android
+function b_icra() {
+    echo 'ionic cordova run android'
+}
+
+## build android
+function b_icba() {
+    echo 'ionic cordova build android'
+}
+
+## run ios
+function b_icri() {
+    echo 'ionic cordova run ios'
+}
+
+## build ios
+function b_icbi() {
+    echo 'ionic cordova build ios'
+}
+
+
